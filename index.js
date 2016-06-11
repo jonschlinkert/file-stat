1251e1a9c4471a3838eea87475055db3a92bdb80
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-11 08:18:01 -0400

    add sync methods

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var utils = require('./utils');
 
 module.exports = function fileStats() {
-  return utils.through.obj(function (file, enc, cb) {
-    getStats(file, function (err, res) {
+  return utils.through.obj(function(file, enc, cb) {
+    lstat(file, function(err, res) {
       if (err) {
         cb(err);
         return;
@@ -14,8 +14,25 @@ module.exports = function fileStats() {
   });
 };
 
-function getStats(file, cb) {
-  utils.fs.lstat(file.path, function (err, stat) {
+/**
+ * Asynchronously add a `stat` property from `fs.stat` to the given file object.
+ *
+ * ```js
+ * var File = require('vinyl');
+ * var stats = require('{%= name %}');
+ * stats.stat(new File({path: 'README.md'}), function(err, file) {
+ *   console.log(file.stat.isFile());
+ *   //=> true
+ * });
+ * ```
+ * @name .stat
+ * @param {Object} `file` File object
+ * @param {Function} `cb`
+ * @api public
+ */
+
+function stat(file, cb) {
+  utils.fs.stat(file.path, function(err, stat) {
     if (err) {
       cb(err);
       return;
@@ -26,7 +43,97 @@ function getStats(file, cb) {
 }
 
 /**
- * Expose `getStats`
+ * Asynchronously add a `lstat` property from `fs.lstat` to the given file object.
+ *
+ * ```js
+ * var File = require('vinyl');
+ * var stats = require('{%= name %}');
+ * stats.lstat(new File({path: 'README.md'}), function(err, file) {
+ *   console.log(file.lstat.isFile());
+ *   //=> true
+ * });
+ * ```
+ * @name .lstat
+ * @param {Object} `file` File object
+ * @param {Function} `cb`
+ * @api public
  */
 
-module.exports.getStats = getStats;
+function lstat(file, cb) {
+  utils.fs.lstat(file.path, function(err, stat) {
+    if (err) {
+      cb(err);
+      return;
+    }
+    file.stat = stat;
+    cb(null, file);
+  });
+}
+
+/**
+ * Synchronously add a `stat` property from `fs.stat` to the given file object.
+ *
+ * ```js
+ * var File = require('vinyl');
+ * var stats = require('{%= name %}');
+ * var file = new File({path: 'README.md'});
+ * stats.statSync(file);
+ * console.log(file.stat.isFile());
+ * //=> true
+ * ```
+ * @name .statSync
+ * @param {Object} `file` File object
+ * @param {Function} `cb`
+ * @api public
+ */
+
+function statSync(file) {
+  var stat;
+  Object.defineProperty(file, 'stat', {
+    configurable: true,
+    set: function(val) {
+      stat = val;
+    },
+    get: function() {
+      return stat || (stat = utils.fs.statSync(this.path));
+    }
+  });
+}
+
+/**
+ * Synchronously add a `lstat` property from `fs.lstat` to the given file object.
+ *
+ * ```js
+ * var File = require('vinyl');
+ * var stats = require('{%= name %}');
+ * var file = new File({path: 'README.md'});
+ * stats.statSync(file);
+ * console.log(file.lstat.isFile());
+ * //=> true
+ * ```
+ * @name .lstatSync
+ * @param {Object} `file` File object
+ * @param {Function} `cb`
+ * @api public
+ */
+
+function lstatSync(file) {
+  var lstat;
+  Object.defineProperty(file, 'lstat', {
+    configurable: true,
+    set: function(val) {
+      lstat = val;
+    },
+    get: function() {
+      return lstat || (lstat = utils.fs.lstatSync(this.path));
+    }
+  });
+}
+
+/**
+ * Expose `stat`
+ */
+
+module.exports.stat = stat;
+module.exports.lstatSync = lstatSync;
+module.exports.statSync = statSync;
--- test.js
+++ test.js
@@ -12,25 +12,41 @@ function streamify(fp) {
   return stream;
 }
 
-describe('add a `stat` property to the given object', function (done) {
-  it('should return a stream:', function () {
+describe('add a `stat` property to the given object', function(cb) {
+  it('should return a stream:', function() {
+    var files = [];
     streamify('README.md')
-      .on('error', console.error)
       .pipe(stats())
-      .on('data', function (file) {
-        assert.equal(typeof file.stat, 'object');
+      .on('data', function(file) {
+        files.push(file);
       })
-      .on('end', function () {
-        done();
+      .on('end', function() {
+        assert.equal(typeof files[0].stat, 'object');
+        assert(files[0].stat.isFile());
+        cb();
       });
   });
 
-  it('should expose `getStats`:', function (done) {
-    stats.getStats({path: 'README.md'}, function (err, file) {
-      if (err) return done(err);
-
+  it('should expose `stat`:', function(cb) {
+    stats.stat({path: 'README.md'}, function(err, file) {
+      if (err) return cb(err);
       assert.equal(typeof file.stat, 'object');
-      done();
+      assert(file.stat.isFile());
+      cb();
     });
+  });
+
+  it('should expose `statSync`:', function() {
+    var file = {path: 'README.md'};
+    stats.statSync(file);
+    assert.equal(typeof file.stat, 'object');
+    assert(file.stat.isFile());
+  });
+
+  it('should expose `lstatSync`:', function() {
+    var file = {path: 'README.md'};
+    stats.lstatSync(file);
+    assert.equal(typeof file.lstat, 'object');
+    assert(file.lstat.isFile());
   });
 });
